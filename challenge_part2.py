839b1d9761409b67c155ed9b743875a0f3c7e4a4
Anna Kondratjeva <an-an-kondratjeva@yandex.ru>
2017-03-06 22:32:20 +0300

    Update challenge_part2.py

--- challenge_part2.py
+++ challenge_part2.py
@@ -2,9 +2,11 @@ import re
 from collections import Counter
 from streamparser import parse
 
-def parse_surface_forms():
+# works only for english, because this is just an example
+
+
+def parse_surface_forms(apertium_string):
     surface_forms = []
-    apertium_string = ('^i/*i$ ^hoped/hope<vblex><past>/hope<vblex><pp>$ ^you/prpers<prn><subj><p2><mf><sp>/prpers<prn><obj><p2><mf><sp>$ ^are/be<vbser><pres>$ ^nice/nice<adj><sint>$')
     parsed_string = parse(apertium_string)
 
     for word in parsed_string:
@@ -12,14 +14,23 @@ def parse_surface_forms():
 
     return surface_forms
 
+
 def count_labels(surface_forms):
-    with open ('en-ud-dev.conllu', 'r', encoding='utf-8') as file:
+    with open('en-ud-train.conllu', 'r', encoding='utf-8') as file:
         corpus = file.read()
 
     for form in surface_forms:
         labels = []
-        regexp = '\n[0-9]+\t' + form + '\t.*?\t.*?\t.*?\t.*?\t.*?\t.*?\t'
-        words = re.findall(regexp, corpus)
+
+        # fixing the register problem
+        low_form_regexp = '\n[0-9]+\t' + \
+            form.lower() + '\t.*?\t.*?\t.*?\t.*?\t.*?\t.*?\t'
+
+        upper_form_regexp = '\n[0-9]+\t' + form[0].upper() + \
+            form[1:] + '\t.*?\t.*?\t.*?\t.*?\t.*?\t.*?\t'
+
+        words = re.findall(low_form_regexp, corpus)
+        words += re.findall(upper_form_regexp, corpus)
 
         for word in words:
             label = word.split('\t')[7]
@@ -28,15 +39,20 @@ def count_labels(surface_forms):
         most_frequent_label = Counter(labels).most_common(1)
 
         if most_frequent_label != []:
-            print ('%s: %s, %s times' %(form, most_frequent_label[0][0], most_frequent_label[0][1]))
+            print(
+                '%s: %s, %s times' %
+                (form,
+                 most_frequent_label[0][0],
+                 most_frequent_label[0][1]))
         else:
-            print (form, ': not found in corpus')
+            print(form, ': not found in corpus')
+
 
 def main():
-    surface_forms = parse_surface_forms()
+    apertium_string = '^cats/cat<n><pl>$ ^are/be<vbser><pres>$ ^pretty/pretty<preadv>/pretty<adj><sint>$'
+    surface_forms = parse_surface_forms(apertium_string)
     count_labels(surface_forms)
+
 
 if __name__ == '__main__':
     main()
-        
-
